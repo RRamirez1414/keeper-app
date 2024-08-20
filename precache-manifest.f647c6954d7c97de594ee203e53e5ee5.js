8b72d41cb96fd454708516da9012eefdec2ff9e9
Ramon Ramirez <ramirez.ramon1414@gmail.com>
2024-08-19 20:32:09 -0600

    Updates

---
+++ precache-manifest.f647c6954d7c97de594ee203e53e5ee5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1322dbdfbf5177b0f88126368314ba27",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "90b14b11084edae8787a",
+    "url": "/keeper-app/static/js/2.9f309bd5.chunk.js"
+  },
+  {
+    "revision": "c8cc816a0e223ffa4a51",
+    "url": "/keeper-app/static/js/main.e2cd8c89.chunk.js"
+  },
+  {
+    "revision": "0acf86f97ee811448167",
+    "url": "/keeper-app/static/js/runtime-main.f0e597c9.js"
+  }
+]);
